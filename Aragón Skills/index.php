92152794ad85091c20cb33a61deb6b26c10b888c
aburrielc <andresbc1997@gmail.com>
2018-04-18 13:25:19 +0200

    Carrousel
    Carrousel

--- Aragón Skills/index.php
+++ Aragón Skills/index.php
@@ -86,6 +86,28 @@
         <div id="content">
             <h2>Películas mejor valoradas</h2>
             
+            <div id="carouselPeliculas" class="carousel slide mx-auto" data-ride="carousel">
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                      <img class="d-block img-fluid m-auto" src="https://ia.media-imdb.com/images/M/MV5BM2RjMmU3ZWItYzBlMy00ZmJkLWE5YzgtNTVkODdhOWM3NGZhXkEyXkFqcGdeQXVyNDA5Mjg5MjA@._V1_UX182_CR0,0,182,268_AL_.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block img-fluid m-auto" src="https://ia.media-imdb.com/images/M/MV5BM2RjMmU3ZWItYzBlMy00ZmJkLWE5YzgtNTVkODdhOWM3NGZhXkEyXkFqcGdeQXVyNDA5Mjg5MjA@._V1_UX182_CR0,0,182,268_AL_.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block img-fluid m-auto" src="https://ia.media-imdb.com/images/M/MV5BM2RjMmU3ZWItYzBlMy00ZmJkLWE5YzgtNTVkODdhOWM3NGZhXkEyXkFqcGdeQXVyNDA5Mjg5MjA@._V1_UX182_CR0,0,182,268_AL_.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselPeliculas" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Anterior</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselPeliculas" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Siguiente</span>
+                </a>
+            </div>
+            
             <div id="busqueda">
                 <!-- Búsqueda -->
                 <form class="form-inline ml-5 mb-3">
